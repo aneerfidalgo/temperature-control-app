50d4bfc03b770615c10d89407c0f7c0b7ec5cfe8
Aneer Fidalgo <aneer@Aneers-Air.hsd1.fl.comcast.net>
2022-07-12 17:34:43 -0400

    implement classnames and buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const App = () => {
-  return <div> hello react! </div>;
+  return (
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div className="temperature-display">10°C </div>
+      </div>
+      <div className="button-container"></div>
+      <button>+</button>
+      <button>-</button>
+    </div>
+  );
 };
 
 export default App;
